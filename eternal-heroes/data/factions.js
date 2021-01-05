18a2ff2038c4b12eab09bc745f79280967bdbbb9
Rusi Papazov <rusi@clippings.com>
2021-01-05 15:31:30 +0200

    Deobfuscate

---
+++ eternal-heroes/data/factions.js
@@ -0,0 +1,26 @@
+const FACTIONS = [
+    {
+        "id": "F",
+        "name": "Fire",
+    },
+    {
+        "id": "T",
+        "name": "Time",
+    },
+    {
+        "id": "J",
+        "name": "Justice",
+    },
+    {
+        "id": "P",
+        "name": "Primal",
+    },
+    {
+        "id": "S",
+        "name": "Shadow",
+    },
+    {
+        "id": "N",
+        "name": "Neutral",
+    },
+];
