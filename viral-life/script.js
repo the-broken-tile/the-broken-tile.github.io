7232c831fc606a65c16f661251be651f2eef049e
Rusi Papazov <rusi@clippings.com>
2021-09-19 01:32:02 +0300

    Add option for more than one generation at a time

--- viral-life/script.js
+++ viral-life/script.js
@@ -4,10 +4,13 @@ const SECTOR_SIZE = 10;
 
 const view = new GameView('container', SIZE, SECTOR_SIZE, CELL_SIZE);
 const $generation = document.getElementById('generation');
-
-$generation.innerHTML = 1;
+const $nextGenerationsCount = document.getElementById('next-generations-count');
+const $nextGenerationButton = document.getElementById('next-generation');
+let nextGenerationsCount = parseInt($nextGenerationsCount.value, 10);
+let generationsLeft;
 
 view.init();
+
 document.getElementById('active-player').addEventListener('change', function (e) {
     e.preventDefault();
     view.updateActivePlayer(parseInt(this.value, 10));
@@ -18,8 +21,26 @@ document.getElementById('reset').addEventListener('click', e => {
     view.reset();
 });
 
-document.getElementById('next-generation').addEventListener('click', e => {
+$nextGenerationButton.addEventListener('click', e => {
     e.preventDefault();
-    view.nextGeneration();
+    generationsLeft = nextGenerationsCount;
+    const tick = () => setTimeout(() => {
+        view.nextGeneration();
+        generationsLeft--;
+        if (generationsLeft) {
+            tick();
+            return;
+        }
+        $nextGenerationButton.disabled = false;
+    }, 500);
+
     $generation.innerHTML = parseInt($generation.innerHTML, 10) + 1;
+    $nextGenerationButton.disabled = true;
+    tick();
+
+});
+
+$nextGenerationsCount.addEventListener('input', e => {
+    e.preventDefault();
+    nextGenerationsCount = parseInt($nextGenerationsCount.value, 10);
 });
