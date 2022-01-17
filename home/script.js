98305027216084ccfaf8056c5f72453b1d74b5ac
Rusi Papazov <rusi@clippings.com>
2022-01-17 10:03:43 +0200

    Merge external links with main links, remove house-rules

--- home/script.js
+++ home/script.js
@@ -2,7 +2,7 @@ const $links = document.getElementById('links');
 
 
 const generatePanel = link => `
-<a class="flexy-panel" href="${link.url}">
+<a class="flexy-panel" href="${link.url}"${link.external ? ' target="_blank" rel="external"' : ''}>
     <h2 class="title">${link.title}</h2>
     <div class="description">${link.description}</div>
 </a>`;
